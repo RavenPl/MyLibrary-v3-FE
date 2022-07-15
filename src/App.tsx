5e7dd9699d313d0932daecb9716aa0a99da8b36a
rossa21 <rossa21@gmail.com>
2022-07-15 22:31:04 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BooksView} from "./views/BooksView";
+import {Route, Routes} from "react-router-dom";
+import {EditBookView} from "./views/EditBookView/EditBookView";
+import {AddBookView} from "./views/AddBookView";
+import {DeleteBookView} from "./views/DeleteBookView";
+import {DeleteAllBooksView} from "./views/DeleteAllBooksView";
+import {NotFoundView} from "./views/NotFoundView/NotFoundView";
+import {SearchBookContext} from "./contexts/SearchBookContext";
+
+export function App() {
+
+    const [search, setSearch] = useState({
+        value: "",
+        category: "title"
+    });
+
+    return (
+
+        <SearchBookContext.Provider value={{
+            search, setSearch
+        }}>
+            <Routes>
+                <Route path="/books" element={<BooksView/>}/>
+                <Route path="/books/edit/:id" element={<EditBookView/>}/>
+                <Route path="/books/add" element={<AddBookView/>}/>
+                <Route path="/books/delete/all" element={<DeleteAllBooksView/>}/>
+                <Route path="/books/delete/:id" element={<DeleteBookView/>}/>
+                <Route path="*" element={<NotFoundView/>}/>
+
+
+            </Routes>
+        </SearchBookContext.Provider>
+    )
 }
 
-export default App;
